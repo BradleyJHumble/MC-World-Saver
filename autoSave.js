efdf7cee2f7f33f941f6feed1069d7b366a2ec67
BradleyJHumble <humble@hey.com>
2020-07-14 23:44:44 -0500

    beta

--- autoSave.js
+++ autoSave.js
@@ -1,22 +1,50 @@
-
 var CronJob = require('cron').CronJob;
-var Config = require('./config.json'); // Config keys
+const octokit = require('@octokit/rest')();
+var Config = require('./config.json'); // Config keys, Very Important!!!
+const simpleGit = require('simple-git')();// Shelljs package for running shell tasks optional
+const shellJs = require('shelljs');// Simple Git with Promise for handling success and failure
+const simpleGitPromise = require('simple-git/promise')();
 
+octokit.authenticate({ // https://github.com/octokit/rest.js
+    type: 'basic',
+    username: Config.userName,
+    password: Config.password
+});// Variables for Repo name and description
 
+shellJs.cd(Config.worldDirectoryLocation); // change current directory to repo directory in local
+
+const gitHubUrl = `https://${Config.userName}:${Config.password}@github.com/${Config.userName}/${Config.repoWorldName}`;
+simpleGit.addConfig('user.email', Config.userEmail); // local git config
+simpleGit.addConfig('user.name', Config.userName); // local git config
+simpleGitPromise.addRemote('origin',gitHubUrl); // Add remote repo url as origin to repo
 
 var job = new CronJob('30 * * * * *', function() { // Currently set at 30 Minutes, pls note first spot is only for minutes. For more Info:  https://www.npmjs.com/package/cron
-console.log('World has ben saved!');
+console.log();
 
+var TimeStamp = Math.floor(Date.now() / 1000); // ts in seconds
 
+simpleGitPromise.add('.') // Add all files for commit
+  .then(
+     (addSuccess) => {
+        console.log(addSuccess);
+     }, (failedAdd) => {
+        console.log('adding files failed');
+  });
 
+simpleGitPromise.commit(TimeStamp) // Commit files with the timestamps
+ .then(
+    (successCommit) => {
+      console.log(successCommit);
+   }, (failed) => {
+      console.log('failed commmit');
+});
 
-
-
-
-
-
-
-
+simpleGitPromise.push('origin','master') // Pushes to repository
+  .then((success) => {
+     console.log('repo successfully pushed' + Config.repoWorldName + ' has been saved!');
+  },(failed)=> {
+     console.log('repo push failed');
+});
 
 }, null, true, 'America/Los_Angeles');
 
--- create.js
+++ create.js
@@ -1,7 +1,5 @@
 const octokit = require('@octokit/rest')();
-var Config = require('./config.json'); // Config keys
-
-// Simple-git without promise
+var Config = require('./config.json'); // Config keys, Very Important!!!
 const simpleGit = require('simple-git')();// Shelljs package for running shell tasks optional
 const shellJs = require('shelljs');// Simple Git with Promise for handling success and failure
 const simpleGitPromise = require('simple-git/promise')();
@@ -12,50 +10,39 @@ octokit.authenticate({ // https://github.com/octokit/rest.js
     password: Config.password
 });// Variables for Repo name and description
 
-// Creates the World Repo
 var description = Config.description
-var name = Config.repoName //Create a Repository online via Github Api
-const createGitHubRepo = await octokit.repos.create(
+var name = Config.repoWorldName //Create a Repository online via Github Api
+const createGitHubRepo = await octokit.repos.create( // Creates the World Repo
    {folderName, repoDescription}
 );
 
-// change current directory to repo directory in local
-shellJs.cd('path/to/repo/folder');
-// Repo name
-const repo = 'dummy';  //Repo name
-// User name and password of your GitHub
-const userName = 'username';
-const password = 'password';
-// Set up GitHub url like this so no manual entry of user pass needed
-const gitHubUrl = `https://${userName}:${password}@github.com/${userName}/${repo}`;
-// add local git config like username and email
-simpleGit.addConfig('user.email','humble@hey.com');
-simpleGit.addConfig('user.name','BradleyJHumble');
-// Add remore repo url as origin to repo
-simpleGitPromise.addRemote('origin',gitHubUrl);
-// Add all files for commit
-  simpleGitPromise.add('.')
+shellJs.cd(Config.worldDirectoryLocation); // change current directory to repo directory in local
+const gitHubUrl = `https://${Config.userName}:${Config.password}@github.com/${Config.userName}/${Config.repoWorldName}`;
+
+simpleGit.addConfig('user.email', Config.userEmail); // local git config
+simpleGit.addConfig('user.name', Config.userName); // local git config
+
+simpleGitPromise.addRemote('origin',gitHubUrl); // Add remote repo url as origin to repo
+
+  simpleGitPromise.add('.') // Add all files for commit
     .then(
        (addSuccess) => {
           console.log(addSuccess);
        }, (failedAdd) => {
           console.log('adding files failed');
     });
-// Commit files as Initial Commit
- simpleGitPromise.commit('Intial commit by MC-World-Saver')
+
+ simpleGitPromise.commit('Intial commit by MC-World-Saver') // Commit files as Initial Commit
    .then(
       (successCommit) => {
         console.log(successCommit);
      }, (failed) => {
         console.log('failed commmit');
  });
-// Finally push to online repository
- simpleGitPromise.push('origin','master')
+
+ simpleGitPromise.push('origin','master') // Pushes to repository
     .then((success) => {
        console.log('repo successfully pushed');
     },(failed)=> {
        console.log('repo push failed');
  });
-
-
- // Inspiration = https://medium.com/@erbalvindersingh/pushing-a-git-repo-online-to-github-via-nodejs-and-simplegit-package-17893ecebddd
